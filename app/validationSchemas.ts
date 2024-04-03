eb6c6baea63008d70631c753ff1a09f7e0e8ccbd
Szymon Tymcio <s.tymcio98@gmail.com>
2024-04-04 00:41:56 +0200

    Build API for updating issues

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,8 +1,8 @@
 import {z} from 'zod'
 
  const issueSchema= z.object({
-    title: z.string().min(2).max(255),
-    description: z.string().min(2)
+    title: z.string().min(2, 'Title is required.').max(255),
+    description: z.string().min(2, 'Description is required.')
 })
 
 export {
